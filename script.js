1578ad84512158e02efd2cc3c7c49464abd2c499
said-ops <idrissisaid29@gmail.com>
2024-07-28 23:21:23 +0000

    form validation

--- script.js
+++ script.js
@@ -1,36 +1,54 @@
-// adding the script to ensure the js runs after the entire HTML document has been loaded
-document.addEventListener("DOMContentLoaded", () => {
-    log.textContent += "load\n";
-})
+//Start with DOMContentLoaded Event:
+document.addEventListener('DOMContentLoaded', function () {
+    //Form Selection:
+    const form = document.getElementById('registration-form');
+    //Feedback Division Selection:
+    const feedbackDiv = document.getElementById('form-feedback');
+    //Form Submission Event Listene
+    form.addEventListener('submit', function (event) {
+        //prevent submission
+        event.preventDefault();
+        //Retrieve User Inputs
+        const userInput = document.getElementById('username');
+        const emailInput = document.getElementById('email');
+        const passInput = document.getElementById('password');
+        //retrieve input value and trim it
+        const username = userInput.value.trim();
+        const email = emailInput.value.trim();
+        const password = passInput.value.trim();
+        //Initialize Validation Variables
+        let isValid = true;
+        let messages = [];
+        //Username Validation
+        if(username.length < 3){
+            isValid = false;
+            messages.push('invalid user name,Try again!');
+        }
+        //email validation
+        if(!email.includes('@') || !email.includes('.')){
+            isValid = false;
+            messages.push('Invalid email,Try again!');
 
-// form Selection
-const form = document.getElementById("registration-form");
-
-// feedbacl division selection
-const feedbackDiv = document.getElementById('form-feedback');
-
-// form submission event listener 
-document.getElementById("form-feedback").addEventListener("click", function(submit){
-    event.preventDefault()
-})
-
-//Retrive User Inputs
-const userName = document.getElementById("userName");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-
-
-const value = userName.value.trim;
-const value = email.value.trim;
-const value = password.value.trim;
+        }
+        //password validation
+        if(password.length <8){
+            isValid = false;
+            messages.push('Invalid password,Try again!')
+        }
+        //Displaying Feedback
+        feedbackDiv.style.display='block';
+        if(isValid){
+            feedbackDiv.textContent = 'Registration successful!';
+            feedbackDiv.style.color = '#28a745';
+        }
+        else{
+            feedbackDiv.innerHTML = messages.join('<br>');
+            feedbackDiv.style.color = '#dc3545';
+        }
+            
+    });
 
 
 
 
- 
-//variable and array declaration
-let isValid = 'false';
-const messages = ["error", "try again", "null", "incorrect"];
-
-//form validation username validation
-
+});
